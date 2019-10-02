e05bf4fa8c0dd1374af310b310b34941710b9322
doubledd99 <doubledd99@outlook.com>
2019-10-02 00:23:49 -0500

    negative portion working, roughly

--- TemperatureOutput/main.cpp
+++ TemperatureOutput/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void scale();
 int input();
+int display();
 
 
 int main() {
@@ -12,12 +13,12 @@ int main() {
 	if (input() == 1) {
 		return 1;
 	}
-
+	display();
 	return 0;
 }
 
 void scale() {
-	cout << "	-30		0		30		60		90		120" << endl;
+	cout << "	-30       0          30          60          90          120" << endl;
 
 }
 
@@ -28,17 +29,43 @@ int input() {
 		cout << "The input is invalid. Please input a number between -30 and 120." << endl;
 		return 1;
 	}
-	if (playerNum < -30 || playerNum > 120) {
-		cout << "That is an invalid temperature please enter a number between -30 and 120." << endl;
+	if (playerNum > 120) {
+		playerNum = 120;
 	}
-	else
+	if (playerNum < -30) {
+		playerNum = -30;
+	}
 	playerNum = int(playerNum);
 	
+	//temp
+	//cout << "playerNum" << playerNum;
+	//temp
 	return 0;
 }
 
 int display() {
+	int numInput;
+	int temperature;
+	string stars;
+	cin >> temperature;
+	numInput = temperature;
+	numInput = numInput / -3;
+	//cout << numInput;
+	while (numInput > 0) {
+		numInput--;
+		stars = stars + "*";
+	}
+	stars.resize(10);
+	//cout << stars << "|";
+	/*string negNum="hello";
+	if (inputfullNum <= -3) {
+		reverse(negNum.begin(), negNum.end());
+		cout << negNum;
+	}*/
+	//cout <<"20" <<"			|									" << endl;
+	reverse(stars.begin(), stars.end());
 
+	cout << temperature << "	" << stars << "|";
 
 	return 0;
 }
