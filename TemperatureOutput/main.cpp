c54a418862c4c966b68f97c50fe525635b85ab1f
Daniel D <doubledd99@outlook.com>
2019-10-02 19:35:04 -0500

    description

--- TemperatureOutput/main.cpp
+++ TemperatureOutput/main.cpp
@@ -1,5 +1,23 @@
+
+/*
+	Daniel Dooley
+	C++ 1st Semester 2019
+	Due: Due Date 10/10/19
+	Lab number 4
+	Description: Design and write a C++ program that inputs a series of hourly temperature
+	from a file, and outputs a bar chart (using stars) of the temperatures for the day. The
+	temperature should be printed to the left of the corresponding bar, and there should be
+	a heading that gives the scale of the chart. The range of temperatures should be from
+	-30 to 120. Because it is hard to display 150 characters on the screen, you should have
+	each star represent a range of 3 degrees. That way, the bars will be at most 50
+	characters wide. Use meaningful variable names, proper indentation, and appropriate
+	comments. Thoroughly test the program using your own data sets. Make sure to vet the
+	values from the input file.
+*/
+
 #include <iostream>
 #include <limits>
+#include <fstream>
 
 using namespace std;
 
