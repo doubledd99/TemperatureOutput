bfb2812e936b501d3334bbda8ec1f4d4a162b05a
doubledd99 <doubledd99@outlook.com>
2019-10-09 22:01:57 -0500

    final product

--- TemperatureOutput/main.cpp
+++ TemperatureOutput/main.cpp
@@ -1,4 +1,3 @@
-
 /*
 	Daniel Dooley
 	C++ 1st Semester 2019
@@ -14,98 +13,93 @@
 	comments. Thoroughly test the program using your own data sets. Make sure to vet the
 	values from the input file.
 */
-
+//Libraries necessary for program to run
 #include <iostream>
 #include <limits>
 #include <fstream>
-
+#include <iomanip>
+//standard namespace is used for cout/ifstream/etc
 using namespace std;
+//Function prototypes
+void scale(); // displays the "-30 to 120" degree scale
+int input(); //inputs the numbers from file. calculates values and outputs to "display()"
+int display(int playerNum); //prints out stars as per the temperature requires to the screen
+bool invalidInput(bool fail); //function that outputs a failed state has occured and closes
 
-void scale();
-int input();
-int display();
-int invalidInput(bool fail);
+int main() { //entry point main function calls other functions when if input == 1 it closes
 
-
-int main() {
-
-	scale();
-	display();
-	//input();
+	scale();//function call
+	if (input() == 1)//function call. if value evaluated is 1 closes with code 1
+		return 1;
+	cout << "Program complete.";
 	return 0;
 }
 
-void scale() {
+void scale() { // displays the "-30 to 120" degree scale
 	cout << "	-30       0         30        60        90      120" << endl;
-
 }
 
-int input() {
-	float playerNum;
-	cin >> playerNum;
-	if (cin.fail()) {
-		cout << "The input is invalid. Please input a number between -30 and 120." << endl;
+int input() { //reads the numbers from file. Calculates values and outputs to "display()" has several failure conditions
+	//bool trueTest = true;
+	float playerNum; //Creates variable to store file inputted values
+	ifstream inFile; //Creates file stream
+	inFile.open("C:/Users/Doubl/source/repos/TemperatureOutput/TemperatureOutput/input.dat"); //Opens file by file path defined
+	if (!inFile) {//If no file is found closes program with message
+		cout << "cannot find file!";
+		return 1;
+	}
+	inFile >> playerNum; //Primes file with first value.
+	while (inFile) {
+		if (playerNum > 120 || playerNum < -30) { // if the number is above or below max and min values it skips that number and tells the user
+			cout << "The number " << playerNum << " is invalid!" << endl;
+			inFile >> playerNum;
+		}
+		else {
+			cout << setprecision(5) << playerNum; //limits max numbers in a decimal
+			playerNum = round(playerNum);//rounds each number to the closest whole number
+			display(playerNum); // function call to display with resultant number from THIS function
+			cout << endl;
+			inFile >> playerNum; //cycles through the list of numbers until EOF
+		}
+	}
+	if (!playerNum) { //if the number becomes invalid program stops with function call to display this info
 		invalidInput(true);
 		return 1;
 	}
-	if (playerNum > 999999) {
-		playerNum = 999999;
-	}
-	if (playerNum < -99999) {
-		playerNum = -99999;
-	}
-	cout << playerNum;
-	if (playerNum > 120) {
-		playerNum = 120;
-	}
-	if (playerNum < -30) {
-		playerNum = -30;
-	}
-	playerNum = int(playerNum);
-	
-	//temp
-	//cout << "playerNum" << playerNum;
-	//temp
-	return playerNum;
-}
 
-int display() {
-	float numInput = input();
-	int temperature;
-	string stars;
-	//cin >> temperature;
-	temperature = numInput;
-	if (numInput < 0) {
-		numInput = round(numInput / -3);
-		while (numInput > 0) {
-			numInput--;
-			stars = stars + "*";
-			
-			
-		}
-		stars.resize(10);		
-		reverse(stars.begin(), stars.end());
-		cout << "	" << stars << "|";
-	}
-	if (numInput > 0) {
-		numInput = round(numInput / 3);
-		while (numInput > 0) {
-			numInput--;
-			stars = stars + "*";
-		}
-		cout << "		  " << "|" << stars;
-	}	
-
-
+	inFile.close();// closes file stream
 	return 0;
 }
 
-int invalidInput(bool fail) {
-	cout << "This shit worked!";
-	if (fail == true) {
-		return 1;
+int display(int playerNum) {//Function in charge of displaying information to screen and calculating stars to display
+	float numInput = playerNum;//assigns local variable to variable inputted by input()
+	string stars; // creates a blank string
+	if (numInput < 0) { // if the number is <0 
+		numInput = round(numInput / -3);//divides by -3, to calculate how many stars should be displayed, in a positive value
+		while (numInput > 0) { //as long as the number of stars is yet to reach 0 
+			numInput--; // subtracts 1 from the number of stars that need to be suffixed on to the string
+			stars = stars + "*"; // suffexes * to the end of the stars string
+		}
+		stars.resize(10);// changes the length of "stars" to 10 regardless of # of stars		
+		reverse(stars.begin(), stars.end()); // flips the string around so that spaces are first and stars last
+		cout << "	" << stars << "|"; // spacing and vertical bar displayed
+	}
+	if (numInput > 0) {//if the number is >0 
+		numInput = round(numInput / 3); //divides by 3 to calculate how many stars should be displayed
+		while (numInput > 0) {//as long as the number of stars is yet to reach 0
+			numInput--; // subtracts 1 from the number of stars that need to be suffixed to the string
+			stars = stars + "*"; // suffixes * to the end of the string
+		}
+		cout << "		  " << "|" << stars; // prints out the vertical bar and spacing before the bar
+	}	
+	return 0;
+}
+
+bool invalidInput(bool fail) {//function that is run each time failure conditions are met (failure conditions defined above)
+	if (fail) {
+		cout << "Please enter a valid input. Closing program";
+		return true;
 	}
 	else
-		return 0;
-	
+		return false;
 }
